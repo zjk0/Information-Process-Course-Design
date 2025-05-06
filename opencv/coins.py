f9f39b053c054a58d1afd833979b6a770095157d
zjk0 <2054295479@qq.com>
2025-05-06 21:21:17 +0800

    second

--- opencv/coins.py
+++ opencv/coins.py
@@ -1,46 +1,84 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("./coins.jpg")
-image = img.copy()
-cv.imshow("img", img)
+def create_mask (shape, bool_index):
+    if len(shape) > 2:
+        shape = (shape[0], shape[1])
 
+    mask = np.zeros(shape, dtype = np.uint8)
+    mask[bool_index] = 255
+
+    return mask
+
+# 读取图像
+img = cv.imread("./coins.jpg")
+img_copy = img.copy()  # 留一个原图像的副本
+
+# 检测图像中的红色区域，也就是背景
 img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 index_red = (((img_hsv[:, :, 0] >= 0) & (img_hsv[:, :, 0] <= 10)) | ((img_hsv[:, :, 0] >= 160) & (img_hsv[:, :, 0] <= 179))) & \
             ((img_hsv[:, :, 1] >= 170) & (img_hsv[:, :, 1] <= 255)) & \
             ((img_hsv[:, :, 2] >= 50) & (img_hsv[:, :, 2] <= 255))
 
+index_gold = ((img_hsv[:, :, 0] >= 11) & (img_hsv[:, :, 0] <= 35)) & \
+             ((img_hsv[:, :, 1] >= 40) & (img_hsv[:, :, 1] <= 255)) & \
+             ((img_hsv[:, :, 2] >= 40) & (img_hsv[:, :, 2] <= 255))
+
+# 得到金色区域掩码
+mask_gold = create_mask(img.shape, index_gold)
+kernel_open = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))
+mask_gold = cv.morphologyEx(mask_gold, cv.MORPH_OPEN, kernel_open)
+kernel_close = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
+mask_gold = cv.morphologyEx(mask_gold, cv.MORPH_CLOSE, kernel_close)
+
+# 得到硬币区域的掩码
 img_hsv[index_red, :] = [0, 0, 0]
 img_no_red = cv.cvtColor(img_hsv, cv.COLOR_HSV2BGR)
-cv.imshow("no_red", img_no_red)
-
 img_gray = cv.cvtColor(img_no_red, cv.COLOR_BGR2GRAY)
-cv.imshow("img_gray", img_gray)
 img_bin = np.zeros_like(img_gray)
 img_bin[img_gray != 0] = 255
 kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
-img_bin = cv.morphologyEx(img_bin, cv.MORPH_CLOSE, kernel)
-cv.imshow("img_bin", img_bin)
+mask_coin = cv.morphologyEx(img_bin, cv.MORPH_CLOSE, kernel)  # 使用闭运算合并一些不连续的小的区域
 
-contours, _ = cv.findContours(img_bin.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-area_list = []
+# 连通域检测，并进行圆逼近
+contours, _ = cv.findContours(mask_coin.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+circle_data = []  # 所有硬币近似圆的数据
 for cnt in contours:
-    area = cv.contourArea(cnt)
-    area_list.append(area)
-
     (x, y), r = cv.minEnclosingCircle(cnt)
     center = (int(x), int(y))
     r = int(r)
-    cv.circle(image, center, r, (0, 255, 0), 2)
+    circle_data.append(np.array([x, y, r], dtype = int))
+    cv.circle(img_copy, center, r, (0, 255, 0), 2)
 
-cv.imshow("circle", image)
+circles = np.array(circle_data)
+cv.imshow("circle", img_copy)
 
-areas = np.array(area_list)
-areas = areas.reshape(-1, 2)
-areas_mean = areas.mean(axis = 1)
-areas_mean = np.sort(areas_mean)
-print(areas_mean)
+# 金色区域连通域检测，并进行圆逼近
+gold_contours, _ = cv.findContours(mask_gold.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+gold_circle_data = []  # 金色硬币近似圆的数据
+for cnt in gold_contours:
+    (x, y), r = cv.minEnclosingCircle(cnt)
+    center = (int(x), int(y))
+    r = int(r)
+    gold_circle_data.append(np.array([x, y, r], dtype = int))
 
-coin_value = np.array([0.01, 0.02, 0.05, 0.1, 0.5])
+gold_circles = np.array(gold_circle_data)
+
+# 硬币数值提取并进行硬币数值总和计算
+value = np.array([0.01, 0.1, 0.02, 0.1, 0.05])
+value_index = 0
+coin_value = np.zeros(circles.shape[0])
+sort_index = np.argsort(circles[:, 2])  # 对第三列进行排序
+sort_circles = circles[sort_index]  # 根据第三列的排序结果，重排数组
+for i in range(circles.shape[0]):
+    x = sort_circles[i, 0]
+    y = sort_circles[i, 1]
+    if (abs(x - gold_circles[0, 0]) < 10 and abs(y - gold_circles[0, 1]) < 10) or (abs(x - gold_circles[1, 0]) < 10 and abs(y - gold_circles[1, 1]) < 10):
+        coin_value[i] = 0.5
+    else:
+        coin_value[i] = value[int(value_index / 2)]
+        value_index += 1
+
+print(f"total: {coin_value.sum() / 2}")
 
 cv.waitKey(0)
--- opencv/red_green_light.py
+++ opencv/red_green_light.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+def create_mask (shape, bool_index):
+    if len(shape) > 2:
+        shape = (shape[0], shape[1])
+
+    mask = np.zeros(shape, dtype = np.uint8)
+    mask[bool_index] = 255
+
+    return mask
+
 # 读取图像
 img = cv.imread("./red_green_light.jpg")
 img_copy = img.copy()  # 留一个原图像的副本
@@ -12,69 +21,81 @@ index_red = (((img_hsv[:, :, 0] >= 0) & (img_hsv[:, :, 0] <= 10)) | ((img_hsv[:,
             ((img_hsv[:, :, 1] >= 50) & (img_hsv[:, :, 1] <= 255)) & \
             ((img_hsv[:, :, 2] >= 50) & (img_hsv[:, :, 2] <= 255))
 
-index_green = ((img_hsv[:, :, 0] >= 35) & (img_hsv[:, :, 0] <= 85)) & \
+index_green = ((img_hsv[:, :, 0] >= 35) & (img_hsv[:, :, 0] <= 90)) & \
               ((img_hsv[:, :, 1] >= 80) & (img_hsv[:, :, 1] <= 255)) & \
               ((img_hsv[:, :, 2] >= 50) & (img_hsv[:, :, 2] <= 255))
 
 index = index_red | index_green  # 红色或绿色的区域
 
-# 非红绿色区域置为黑色
-img_hsv[~index, :] = [0, 0, 0]
-img_hsv = cv.cvtColor(img_hsv, cv.COLOR_HSV2BGR)
-# cv.imshow("hsv process", img_hsv)
-
-# 制作一个掩码，将红绿色区域提取出来
-mask = np.zeros_like(img)
-mask[index] = [255, 255, 255]
-# cv.imshow("mask", mask)
-
 # 边缘检测
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # 原图像转换为灰度图像
-# cv.imshow("gray", img_gray)
 img_gray = cv.medianBlur(img_gray, 3)  # 进行中值滤波使得图像更加平滑
-# cv.imshow("blur", img_gray)
 img_edge = cv.Canny(img_gray, 150, 220)  # Canny边缘检测
-# cv.imshow("edge", img_edge)
 
 # 将不是灯的边缘去掉
-mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+mask = create_mask(img.shape, index)  # 红绿灯区域的掩码
 kernel_dilate = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
 mask = cv.dilate(mask, kernel_dilate, iterations = 1)
 img_masked = cv.bitwise_and(img_edge, mask)
-# cv.imshow("edge masked", img_masked)
 
-# 将灯的边缘转换为一个边缘
+# 将灯的边缘转换为一个边缘，而不是一种类似于环形的结构
 kernel_closing = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
 img_masked = cv.morphologyEx(img_masked, cv.MORPH_CLOSE, kernel_closing)
-# cv.imshow("edge masked closing", img_masked)
 
 # 连通域检测并且使用圆逼近
 img_masked_copy = img_masked.copy()
-contours, _ = cv.findContours(img_masked_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-center_list = []
+contours, _ = cv.findContours(img_masked_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # 检测最外围的边缘
+center_list = []  # 圆心列表
 for cnt in contours:
-    (x, y), r = cv.minEnclosingCircle(cnt)
+    (x, y), r = cv.minEnclosingCircle(cnt)  #得到近似圆的中心和半径
     center = (int(x), int(y))
     center_list.append(np.array(center))
     r = int(r)
     cv.circle(img_copy, center, r, (0, 0, 255), 2)
 
-cv.imshow("circle", img_copy)
 centers = np.array(center_list)
-print(centers)
+cv.imshow("with circle", img_copy)
 
+# 重塑数组
+centers = centers[::-1]
+centers = centers.reshape(3, -1, 2)
+sort_index = np.argsort(centers[:, :, 0], axis = 1)
+row_index = np.arange(centers.shape[0])[:, None]
+centers = centers[row_index, sort_index, :]
+centers = centers.reshape(-1, 2)
 
-# 灯亮区域
-# index_light = index & ((img_hsv[:, :, 2] >= 230) & (img_hsv[:, :, 2] <= 255))
+# 检测颜色和亮度
+lights_color = np.chararray(centers.shape[0])
+for i in range(centers.shape[0]):
+    row = centers[i, 1]
+    column = centers[i, 0]
+    h = img_hsv[row, column, 0]
+    s = img_hsv[row, column, 1]
 
-# mask_light = np.zeros_like(img)
-# mask_light[index_light] = [255, 255, 255]
-# cv.imshow("mask_light", mask_light)
+    # 处理颜色跳变情况，只对绿色处理
+    if i > 0 and i < centers.shape[0] - 1:
+        if not ((h >= 0 and h <= 10) or (h >= 160 and h <= 179)):
+            h1 = int(img_hsv[centers[i - 1, 1], centers[i - 1, 0], 0])
+            h2 = int(img_hsv[centers[i + 1, 1], centers[i + 1, 0], 0])
+            if (abs(h - h1) > 30) and (abs(h - h2) > 30):
+                h = int((h1 + h2) / 2)
 
-# img_gray_blur = cv.medianBlur(img_gray, 3)
-# mask_light_2 = np.zeros_like(img_gray)
-# mask_light_2[img_gray_blur > 200] = 255
-# cv.imshow("mask_light_2", mask_light_2)
+    if h >= 35 and h <= 90:
+        if s < 150:
+            lights_color[i] = 'G'
+        else:
+            lights_color[i] = 'g'
+    elif (h >= 0 and h <= 10) or (h >= 160 and h <= 179):
+        if s < 50:
+            lights_color[i] = 'R'
+        else:
+            lights_color[i] = 'r'
+    else:
+        lights_color[i] = 'N'
 
 print("r/R: 红色\ng/G: 绿色\n小写字母表示灯灭, 大写字母表示灯亮")
+lights_color = np.array(lights_color)
+lights_color = lights_color.reshape(3, -1)
+print(lights_color)
+
 cv.waitKey(0)
